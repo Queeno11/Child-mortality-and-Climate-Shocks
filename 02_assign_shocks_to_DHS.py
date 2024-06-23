70bc76caa3a5988a3c6734451b3adf4546636fef
Queeno11 <abbatenicolas@gmail.com>
2024-06-23 18:51:20 -0300

    refactoring for faster implementation
    improved function get_climate_shock() speed and using swifter to run in parallel (dask)

--- 02_assign_shocks_to_DHS.py
+++ 02_assign_shocks_to_DHS.py
@@ -1,203 +1,208 @@
 import os
+import logging
 import xarray as xr
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from tqdm import tqdm  # for notebooks
+from dask.distributed import Client
+import swifter
 
-pd.options.mode.chained_assignment = None  # default='warn'
-tqdm.pandas()
+if __name__ == "__main__":
 
-# Set global variables
-PROJECT = r"Z:\Laboral\World Bank\Paper - Child mortality and Climate Shocks"
-OUTPUTS = rf"{PROJECT}\Outputs"
-DATA = rf"{PROJECT}\Data"
-DATA_IN = rf"{DATA}\Data_in"
-DATA_PROC = rf"{DATA}\Data_proc"
-DATA_OUT = rf"{DATA}\Data_out"
-
-### Load data #############
-climate_data = xr.open_dataset(rf"{DATA_OUT}/Climate_shocks_v4.nc")
-dates = climate_data.time.values
-
-full_dhs = pd.read_stata(rf"{DATA_IN}/DHS/DHSBirthsGlobalAnalysis_04172024.dta")
-full_dhs["ID"] = full_dhs.index
-df = full_dhs.copy()
-###########################
-
-climate_variables = ["spi1", "spi3", "spi6", "spi9", "spi12", "t", "std_t", "stdm_t"]
-
-
-def get_climate_shock(from_date, to_date, lat, lon):
-    if pd.isna(from_date):
-        return np.nan
-
-    # Filter point
-    point_data = climate_data.sel(time=slice(from_date, to_date)).sel(
-        lat=lat, lon=lon, method="nearest"
+    swifter.set_defaults(
+        force_parallel=True,
     )
 
-    # Get position of original data
-    lat = point_data.lat.item()
-    lon = point_data.lon.item()
+    pd.options.mode.chained_assignment = None  # default='warn'
 
-    # Filter by time
-    inutero = point_data.isel(time=slice(0, 8))
-    born_1m = point_data.isel(time=9)
-    born_2to12m = point_data.isel(time=slice(10, 21))
+    logging.getLogger("distributed").setLevel(logging.WARNING)
 
-    out_vars = [lat, lon]
-    for spi in climate_variables:
-        # Compute mean values for SPI
-        inutero_mean = inutero[spi].mean().item()
-        born_1m_mean = born_1m[spi].mean().item()
-        born_2to12m_mean = born_2to12m[spi].mean().item()
-        # Compute min values for SPI
-        inutero_min = inutero[spi].min().item()
-        born_1m_min = born_1m[spi].min().item()
-        born_2to12m_min = born_2to12m[spi].min().item()
-        # Compute max values for SPI
-        inutero_max = inutero[spi].max().item()
-        born_1m_max = born_1m[spi].max().item()
-        born_2to12m_max = born_2to12m[spi].max().item()
+    # Initialize Dask client
+    client = Client()
 
-        out_vars_this_spi = [
-            inutero_mean,
-            born_1m_mean,
-            born_2to12m_mean,
-            inutero_min,
-            born_1m_min,
-            born_2to12m_min,
-            inutero_max,
-            born_1m_max,
-            born_2to12m_max,
+    # Set global variables
+    PROJECT = r"Z:\Laboral\World Bank\Paper - Child mortality and Climate Shocks"
+    OUTPUTS = rf"{PROJECT}\Outputs"
+    DATA = rf"{PROJECT}\Data"
+    DATA_IN = rf"{DATA}\Data_in"
+    DATA_PROC = rf"{DATA}\Data_proc"
+    DATA_OUT = rf"{DATA}\Data_out"
+
+    ### Load data #############
+    print("Loading data...")
+    climate_data = xr.open_dataset(rf"{DATA_OUT}/Climate_shocks_v4.nc")
+    dates = climate_data.time.values
+
+    full_dhs = pd.read_stata(rf"{DATA_IN}/DHS/DHSBirthsGlobalAnalysis_04172024.dta")
+    full_dhs["ID"] = full_dhs.index
+    df = full_dhs.copy()
+    print("Data loaded! Processing...")
+    ###########################
+
+    climate_variables = [
+        "spi1",
+        "spi3",
+        "spi6",
+        "spi9",
+        "spi12",
+        "t",
+        "std_t",
+        "stdm_t",
+    ]
+
+    def get_climate_shock(from_date, to_date, lat, lon):
+        if pd.isna(from_date):
+            return np.nan
+
+        # Filter point
+        climate_data_vars = climate_data[climate_variables]
+        point_data = climate_data_vars.sel(time=slice(from_date, to_date)).sel(
+            lat=lat, lon=lon, method="nearest"
+        )
+
+        # Get position of original data
+        lat = point_data.lat.item()
+        lon = point_data.lon.item()
+
+        # Filter by time
+        inutero = point_data.isel(time=slice(0, 8))
+        born_1m = point_data.isel(time=slice(9, 10))  # 10th month == 1st month of life
+        born_2to12m = point_data.isel(time=slice(10, 21))
+
+        out = [lat, lon]
+        for ds_time in [inutero, born_1m, born_2to12m]:
+            time_avg = ds_time.mean(dim="time").to_array().values
+            time_min = ds_time.min(dim="time").to_array().values
+            time_max = ds_time.max(dim="time").to_array().values
+            out = np.concatenate([out, time_avg, time_min, time_max])
+
+        return out.tolist()
+
+    ### Process dataframe ####
+    # Create datetime object from year and month
+    df["day"] = 1
+    df["month"] = df["chb_month"].astype(float)
+    df["year"] = df["chb_year"].astype(float)
+    df["birthdate"] = pd.to_datetime(df[["year", "month", "day"]]).to_numpy()
+
+    # Maximum range of dates
+    df["from_date"] = df["birthdate"] + pd.DateOffset(
+        months=-9
+    )  # From in utero (9 months before birth)
+    df["to_date"] = df["birthdate"] + pd.DateOffset(
+        months=13
+    )  # To the first year of life
+
+    # Filter children from_date greater than 1990 (we only have climate data from 1990)
+    df = df[df["from_date"] > "1990-01-01"]
+
+    # # Construct deathdate variable
+    # df["deathdate"] = df[df["child_agedeath"]<=12].progress_apply(lambda x: x["birthdate"] + pd.DateOffset(months=x["child_agedeath"]), axis=1)
+
+    # # Replace to_date with deathdate if the child died
+    # df["to_date"] = np.where((df["child_agedeath"]<=12) & (df["deathdate"]<df["to_date"]), df["deathdate"], df["to_date"])
+
+    # Filter children to_date smalle than 2021 (we only have climate data to 2020)
+    df = df[df["to_date"] < "2021-01-01"]
+
+    ### Run process ####
+    coords_cols = ["lat_climate", "lon_climate"]
+    shock_cols = []
+    for name in climate_variables:
+        shock_cols += [
+            f"{name}_inutero_avg",
+            f"{name}_inutero_min",
+            f"{name}_inutero_max",
+            f"{name}_30d_mean",
+            f"{name}_30d_min",
+            f"{name}_30d_max",
+            f"{name}_2m12m_mean",
+            f"{name}_2m12m_min",
+            f"{name}_2m12m_max",
         ]
-        out_vars += out_vars_this_spi
+    all_cols = coords_cols + shock_cols
 
-    return out_vars
+    print("Assigning climate shocks to DHS data...")
+    for n in tqdm(range(0, df.ID.max(), 10_000)):
+        if os.path.exists(rf"{DATA_PROC}/births_climate_{n}.csv"):
+            print(f"births_climate_{n}.csv exists, moving to next iteration")
+            continue
+        chunk = df.loc[
+            (df.ID >= n) & (df.ID < n + 10_000),
+            ["ID", "from_date", "to_date", "LATNUM", "LONGNUM"],
+        ].copy()
+        if chunk.shape[0] == 0:
+            continue
+        climate_results = chunk.swifter.apply(
+            lambda s: get_climate_shock(
+                s["from_date"], s["to_date"], s["LATNUM"], s["LONGNUM"]
+            ),
+            axis=1,
+        )
+        climate_results = climate_results.apply(pd.Series)
+        climate_results.columns = all_cols
+        climate_results["ID"] = chunk["ID"]
+        climate_results.to_csv(rf"{DATA_PROC}/births_climate_{n}.csv", index=False)
 
+    files = os.listdir(rf"{DATA_PROC}")
+    files = [f for f in files if f.startswith("births_climate_")]
+    data = []
+    for file in tqdm(files):
+        df = pd.read_csv(rf"{DATA_PROC}/{file}")
+        data += [df]
+    df = pd.concat(data)
 
-### Process dataframe ####
-# Create datetime object from year and month
-df["day"] = 1
-df["month"] = df["chb_month"].astype(float)
-df["year"] = df["chb_year"].astype(float)
-df["birthdate"] = pd.to_datetime(df[["year", "month", "day"]]).to_numpy()
+    ####### Process data:
+    df = full_dhs.merge(df, on="ID", how="inner")
+    print("Number of observations merged with climate data:", df.shape[0])
+    df = df.dropna(subset=["v008", "chb_year", "chb_month"], how="any")
 
-# Maximum range of dates
-df["from_date"] = df["birthdate"] + pd.DateOffset(
-    months=-9
-)  # From in utero (9 months before birth)
-df["to_date"] = df["birthdate"] + pd.DateOffset(months=13)  # To the first year of life
+    # Date of interview
+    df["year"] = 1900 + (df["v008"] - 1) // 12
+    df["month"] = df["v008"] - 12 * (df["year"] - 1900)
+    df["day"] = 1
+    df["interview_date"] = pd.to_datetime(df[["year", "month", "day"]], dayfirst=False)
+    df["interview_year"] = df["year"]
+    df["interview_month"] = df["month"]
+    df = df.drop(columns=["year", "month", "day"])
 
-# Filter children from_date greater than 1990 (we only have climate data from 1990)
-df = df[df["from_date"] > "1990-01-01"]
+    # Date of birth
+    df["year"] = df["chb_year"].astype(int)
+    df["month"] = df["chb_month"].astype(int)
+    df["day"] = 15
+    df["birth_date"] = pd.to_datetime(df[["year", "month", "day"]], dayfirst=False)
+    df = df.drop(columns=["year", "month", "day"])
 
-# # Construct deathdate variable
-# df["deathdate"] = df[df["child_agedeath"]<=12].progress_apply(lambda x: x["birthdate"] + pd.DateOffset(months=x["child_agedeath"]), axis=1)
+    # Number of days from interview
+    df["days_from_interview"] = df["interview_date"] - df["birth_date"]
 
-# # Replace to_date with deathdate if the child died
-# df["to_date"] = np.where((df["child_agedeath"]<=12) & (df["deathdate"]<df["to_date"]), df["deathdate"], df["to_date"])
-
-# Filter children to_date smalle than 2021 (we only have climate data to 2020)
-df = df[df["to_date"] < "2021-01-01"]
-
-
-### Run process ####
-coords_cols = ["lat_climate", "lon_climate"]
-shock_cols = []
-for name in climate_variables:
-    shock_cols += [
-        f"{name}_inutero_avg",
-        f"{name}_inutero_min",
-        f"{name}_inutero_max",
-        f"{name}_30d_mean",
-        f"{name}_30d_min",
-        f"{name}_30d_max",
-        f"{name}_2m12m_mean",
-        f"{name}_2m12m_min",
-        f"{name}_2m12m_max",
-    ]
-all_cols = coords_cols + shock_cols
-
-for n in tqdm(range(0, df.ID.max(), 10_000)):
-    if os.path.exists(rf"{DATA_PROC}/births_climate_{n}.csv"):
-        print(f"births_climate_{n}.csv exists, moving to next iteration")
-        continue
-    chunk = df.loc[
-        (df.ID >= n) & (df.ID < n + 10_000),
-        ["ID", "from_date", "to_date", "LATNUM", "LONGNUM"],
-    ].copy()
-    if chunk.shape[0] == 0:
-        continue
-    climate_results = chunk[["from_date", "to_date", "LATNUM", "LONGNUM"]].apply(
-        lambda s: get_climate_shock(
-            s["from_date"], s["to_date"], s["LATNUM"], s["LONGNUM"]
-        ),
-        axis=1,
+    # excluir del análisis a aquellos niños que nacieron 12 meses alrededor de la fecha de la encuesta y no más allá de 10 y 15 años del momento de la encuesta.
+    # PREGUNTA PARA PAULA: ¿ella ya hizo el filtro de 15 años y 30 dias?
+    df["last_15_years"] = (df["days_from_interview"] > np.timedelta64(30, "D")) & (
+        df["days_from_interview"] < np.timedelta64(15 * 365, "D")
     )
-    climate_results = climate_results.apply(pd.Series)
-    climate_results.columns = all_cols
-    climate_results["ID"] = chunk["ID"]
-    climate_results.to_csv(rf"{DATA_PROC}/births_climate_{n}.csv", index=False)
-
-files = os.listdir(rf"{DATA_PROC}")
-files = [f for f in files if f.startswith("births_climate_")]
-data = []
-for file in tqdm(files):
-    df = pd.read_csv(rf"{DATA_PROC}/{file}")
-    data += [df]
-df = pd.concat(data)
-
-####### Process data:
-df = full_dhs.merge(df, on="ID", how="inner")
-print("Number of observations merged with climate data:", df.shape[0])
-df = df.dropna(subset=["v008", "chb_year", "chb_month"], how="any")
-
-# Date of interview
-df["year"] = 1900 + (df["v008"] - 1) // 12
-df["month"] = df["v008"] - 12 * (df["year"] - 1900)
-df["day"] = 1
-df["interview_date"] = pd.to_datetime(df[["year", "month", "day"]], dayfirst=False)
-df["interview_year"] = df["year"]
-df["interview_month"] = df["month"]
-df = df.drop(columns=["year", "month", "day"])
-
-# Date of birth
-df["year"] = df["chb_year"].astype(int)
-df["month"] = df["chb_month"].astype(int)
-df["day"] = 15
-df["birth_date"] = pd.to_datetime(df[["year", "month", "day"]], dayfirst=False)
-df = df.drop(columns=["year", "month", "day"])
-
-# Number of days from interview
-df["days_from_interview"] = df["interview_date"] - df["birth_date"]
-
-# excluir del análisis a aquellos niños que nacieron 12 meses alrededor de la fecha de la encuesta y no más allá de 10 y 15 años del momento de la encuesta.
-# PREGUNTA PARA PAULA: ¿ella ya hizo el filtro de 15 años y 30 dias?
-df["last_15_years"] = (df["days_from_interview"] > np.timedelta64(30, "D")) & (
-    df["days_from_interview"] < np.timedelta64(15 * 365, "D")
-)
-df["last_10_years"] = (df["days_from_interview"] > np.timedelta64(30, "D")) & (
-    df["days_from_interview"] < np.timedelta64(10 * 365, "D")
-)
-df["since_2003"] = df["interview_year"] >= 2003
-df = df[df["last_15_years"] == True]
-####### Export data:
-# df.to_parquet(rf"{DATA_PROC}/ClimateShocks_assigned_v3.parquet")
-df = df[
-    all_cols
-    + [
-        "ID",
-        "interview_year",
-        "interview_month",
-        "birth_date",
-        "last_15_years",
-        "last_10_years",
-        "since_2003",
+    df["last_10_years"] = (df["days_from_interview"] > np.timedelta64(30, "D")) & (
+        df["days_from_interview"] < np.timedelta64(10 * 365, "D")
+    )
+    df["since_2003"] = df["interview_year"] >= 2003
+    df = df[df["last_15_years"] == True]
+    ####### Export data:
+    # df.to_parquet(rf"{DATA_PROC}/ClimateShocks_assigned_v3.parquet")
+    df = df[
+        all_cols
+        + [
+            "ID",
+            "interview_year",
+            "interview_month",
+            "birth_date",
+            "last_15_years",
+            "last_10_years",
+            "since_2003",
+        ]
     ]
-]
 
-# Drop nans in spi/temp values
-df = df.dropna(subset=shock_cols, how="any")
-df.to_stata(rf"{DATA_PROC}\ClimateShocks_assigned_v4.dta")
+    # Drop nans in spi/temp values
+    df = df.dropna(subset=shock_cols, how="any")
+    df.to_stata(rf"{DATA_PROC}\ClimateShocks_assigned_v4.dta")
+    print(f"Data ready! file saved at {DATA_PROC}/ClimateShocks_assigned_v4.dta")
